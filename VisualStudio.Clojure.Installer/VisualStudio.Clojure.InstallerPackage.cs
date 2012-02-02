698585adeef466ec3ed668e8e7e7b5850e0619f2
jmis <jmis.dev@gmail.com>
2012-02-01 23:57:48 -0500

    Removed the download process.  Users will only be prompted to uninstall the deprecated version of vsClojure.

--- VisualStudio.Clojure.Installer/VisualStudio.Clojure.InstallerPackage.cs
+++ VisualStudio.Clojure.Installer/VisualStudio.Clojure.InstallerPackage.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.ExtensionManager;
@@ -12,87 +15,37 @@ namespace vsClojure.VisualStudio_Clojure_Installer
 	[ProvideAutoLoad(UIContextGuids80.NoSolution)]
 	public sealed class VisualStudio_Clojure_InstallerPackage : Package
 	{
-		public RepositoryEntry ExtensionRepositoryEntry =
-			new RepositoryEntry()
-			{
-				DownloadUrl = "http://visualstudiogallery.msdn.microsoft.com/8938e63c-7527-443b-956d-84597b9bfc6f/file/64178/2/ClojureExtension.Deployment.vsix",
-				Name = "vsClojure",
-				VsixReferences = ""
-			};
-
 		protected override void Initialize()
 		{
 			base.Initialize();
 			var dte = (DTE2) GetService(typeof (DTE));
-			dte.Events.DTEEvents.OnStartupComplete += Download;
+			dte.Events.DTEEvents.OnStartupComplete += UninstallPrompt;
 		}
 
-		private void Download()
+		private static void UninstallPrompt()
 		{
 			var prompt = new InstallationPrompt();
-
-			prompt.InstallNowButton.Click +=
-				(sender, args) =>
-				{
-					prompt.Close();
-					InstallExtension();
-				};
-
 			prompt.RemindMeLaterButton.Click += (sender, args) => prompt.Close();
+			prompt.Remove.Click += (sender, args) => prompt.Close();
+			prompt.Remove.Click += (sender, args) => UninstallOldVersion();
 			prompt.ShowDialog();
 		}
 
-		private void InstallExtension()
+		private static void UninstallOldVersion()
 		{
-			var progressWindow = new InstallProgress();
-			var extensionManager = GetGlobalService(typeof(SVsExtensionManager)) as IVsExtensionManager;
-			var extensionRepository = GetGlobalService(typeof(SVsExtensionRepository)) as IVsExtensionRepository;
-			
-			progressWindow.RestartNowButton.IsEnabled = false;
-
-			progressWindow.RestartNowButton.Click +=
-				(sender, args) =>
-				{
-					progressWindow.Close();
-					RestartVisualStudio();
-				};
-
-			extensionRepository.DownloadProgressChanged +=
-				(sender, args) =>
-				{
-					progressWindow.CurrentStepText.Text = "Downloading...";
-					progressWindow.DownloadPercentComplete.Minimum = 0;
-					progressWindow.DownloadPercentComplete.Value = args.BytesReceived;
-					progressWindow.DownloadPercentComplete.Maximum = args.TotalBytesToReceive;
-					progressWindow.DownloadedAmountText.Text = ToKilobytesDisplay(args.BytesReceived) + " / " + ToKilobytesDisplay(args.TotalBytesToReceive);
-				};
-
-			extensionRepository.DownloadCompleted +=
-				(sender, args) =>
-				{
-					progressWindow.CurrentStepText.Text = "Installing...";
-					var installerExtension = extensionManager.GetInstalledExtension("40953a10-3425-499c-8162-a90059792d13");
-					extensionManager.Install(args.Payload, false);
-					extensionManager.Enable(extensionManager.GetInstalledExtension("7712178c-977f-45ec-adf6-e38108cc7739"));
-					extensionManager.Disable(installerExtension);
-					extensionManager.Uninstall(installerExtension);
-					progressWindow.RestartNowButton.IsEnabled = true;
-					progressWindow.CurrentStepText.Text = "Installation complete.";
-				};
-
-			extensionRepository.DownloadAsync(ExtensionRepositoryEntry);
-			progressWindow.ShowDialog();
-		}
-
-		private void RestartVisualStudio()
-		{
-			var shell = (IVsShell4)GetService(typeof(SVsShell));
-			shell.Restart((uint)__VSRESTARTTYPE.RESTART_Normal);
-		}
-
-		private static string ToKilobytesDisplay(long bytes)
-		{
-			return (bytes / 1024) + "k";
+			try
+			{
+				var extensionManager = GetGlobalService(typeof(SVsExtensionManager)) as IVsExtensionManager;
+				var installerExtension = extensionManager.GetInstalledExtension("40953a10-3425-499c-8162-a90059792d13");
+				extensionManager.Disable(installerExtension);
+				extensionManager.Uninstall(installerExtension);
+			}
+			catch (Exception e)
+			{
+				var message = new StringBuilder();
+				message.Append("Failed to remove the deprecated version of vsClojure.  Please use the extension manager to manually uninstall.");
+				MessageBox.Show(message.ToString(), "vsClojure Uninstall Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
 		}
 	}
 }
